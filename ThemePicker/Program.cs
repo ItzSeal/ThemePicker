77b081956d820abcad3cc6d8c3b2c1cd9b772e30
Jordan <38435668+ItzSeal@users.noreply.github.com>
2020-10-01 17:38:48 +0100

    Added C# Files
    These are the finished files from Episode 1 of Game Development.

---
+++ ThemePicker/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ThemePicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Theme Picker!"; // Sets the title of the console.
+            ThemeHandler.Load(); // Loads the themes from file
+            Menu(); // Draws the menu to the screen.
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("/-------------------\\");
+            Console.WriteLine("|        MENU       |");
+            Console.WriteLine("\\-------------------/");
+            Console.WriteLine("");
+            Console.WriteLine("1) Get Theme(s)");
+            Console.WriteLine("2) List Themes");
+            Console.WriteLine("3) Add Theme");
+            Console.WriteLine("4) Remove Theme");
+            Console.WriteLine("5) Exit");
+            Console.WriteLine("");
+
+            int _value = int.Parse(Console.ReadLine()); // Gets value from user.
+
+            switch (_value)
+            {
+                case 1:
+                    GetThemes(); // Get one or more themes
+                    return;
+
+                case 2:
+                    ListThemes(); // List all the themes
+                    return;
+
+                case 3:
+                    AddTheme(); // Add a theme
+                    return;
+
+                case 4:
+                    RemoveTheme(); // Remove a theme
+                    return;
+
+                case 5:
+                    Environment.Exit(0); // Exit the programme
+                    break;
+            }
+
+            // The user entered a value that is not valid
+            Console.Clear(); // Clear the screen
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Unknown Input... Try again...");
+            Console.ForegroundColor = ConsoleColor.White;
+            Menu(); // Draws the menu to the screen.
+        }
+
+        public static void ListThemes()
+        {
+            Console.Clear();
+            Console.WriteLine("/-------------------\\");
+            Console.WriteLine("|       THEMES      |");
+            Console.WriteLine("\\-------------------/");
+            Console.WriteLine("");
+
+            for(int  i = 0; i < ThemeHandler.Themes.Count; i++) // Go through all the different themes
+            {
+                Console.WriteLine($"{i}) {ThemeHandler.Themes[i].ThemeName}"); // Write themes to screen
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+            Menu(); // Draws the menu to the screen.
+        }
+
+        public static void RemoveTheme()
+        {
+            Console.Clear();
+            Console.WriteLine("/-------------------\\");
+            Console.WriteLine("|   REMOVE THEMES   |");
+            Console.WriteLine("\\-------------------/");
+            Console.WriteLine("");
+
+            for (int i = 0; i < ThemeHandler.Themes.Count; i++) // Go through all the different themes
+            {
+                Console.WriteLine($"{i}) {ThemeHandler.Themes[i].ThemeName}"); // Write themes to screen
+            }
+
+            int _value = int.Parse(Console.ReadLine()); // Get number of the theme that should be deleted
+
+            if (ThemeHandler.Themes[_value] != null)
+                ThemeHandler.RemoveTheme(ThemeHandler.Themes[_value]); // Remove said theme
+            else
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Unknown Input... Try again...");
+                Console.ForegroundColor = ConsoleColor.White;
+                RemoveTheme();
+            }
+
+            Console.Clear();
+            Menu(); // Draws the menu to the screen.
+
+        }
+
+        public static void AddTheme()
+        {
+            Console.Clear();
+            Console.WriteLine("/-------------------\\");
+            Console.WriteLine("|     ADD THEMES    |");
+            Console.WriteLine("\\-------------------/");
+            Console.WriteLine("");
+            Console.WriteLine("What is the theme you wish to add?");
+            Console.WriteLine("");
+
+            string _themeName = Console.ReadLine(); // Theme that should be added
+
+            ThemeHandler.AddTheme(new Theme(_themeName)); // Add the theme
+
+            Console.Clear();
+            Console.WriteLine("/-------------------\\");
+            Console.WriteLine("|    BACK TO MENU?   |");
+            Console.WriteLine("\\-------------------/");
+            Console.WriteLine("");
+            Console.WriteLine("(Y / N)");
+            Console.WriteLine("");
+
+            string _answer = Console.ReadKey().ToString().ToLower(); // Should the programme go back to the menu?
+
+            if (_answer == "y")
+            {
+                Console.Clear();
+                Menu(); // Draws the menu to the screen.
+            }
+
+            Console.Clear();
+            AddTheme(); // Add another theme
+        }
+
+        public static void GetThemes()
+        {
+            Console.Clear();
+            Console.WriteLine("How many themes do you require?"); // Get how many themes the user requires.
+            int _value = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("/-------------------\\");
+            Console.WriteLine("|  YOUR THEMES ARE  |");
+            Console.WriteLine("\\-------------------/");
+            Console.WriteLine("");
+            for(int i = 1; i <= _value; i++) // lists the themes
+            {
+                var _random = new Random();
+                Theme _theme = ThemeHandler.Themes[_random.Next(0, ThemeHandler.Themes.Count)];
+                Console.WriteLine($"{i}) {_theme.ThemeName}");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Press ANY key to go back to the menu...");
+            Console.ReadKey();
+            Console.Clear();
+            Menu(); // Draws the menu to the screen.
+        }
+    }
+}
---
+++ ThemePicker/ThemeHandler.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Text;
+
+namespace ThemePicker
+{
+    class ThemeHandler
+    {
+        private static List<Theme> _themes;
+        private static string _filePath = Directory.GetCurrentDirectory() + "/themes.txt";
+
+        public static List<Theme> Themes { get { return _themes; } }
+
+        /// <summary>
+        /// Load every <seealso cref="Theme"/> from a text file
+        /// </summary>
+        public static void Load()
+        {
+            string[] _lines = LoadThemes();
+            _themes = new List<Theme>();
+
+            foreach (string _line in _lines) // For each line.
+            {
+                Theme _theme = new Theme(_line); // Create a theme with the text of the line.
+                _themes.Add(_theme); // Add to the list.
+            }
+        }
+
+        /// <summary>
+        /// Removes a <see cref="Theme"/> from the complete list.
+        /// </summary>
+        /// <param name="_theme">The <see cref="Theme"/> to be removed</param>
+        public static void RemoveTheme(Theme _theme)
+        {
+            _themes.Remove(_theme);
+            SaveThemes();
+        }
+
+        /// <summary>
+        /// Saves every <see cref="Theme"/> to the text file.
+        /// </summary>
+        private static void SaveThemes()
+        {
+            string _save = "";
+
+            foreach(Theme _theme in _themes)
+            {
+                _save += _theme.ThemeName + "\n";
+            }
+
+            StreamWriter _streamWritter = new StreamWriter(_filePath);
+            _streamWritter.Write(_save);
+            _streamWritter.Close();
+        }
+
+        /// <summary>
+        /// Loads the text file and split it into a seperate <see cref="string"/>
+        /// </summary>
+        /// <returns>Array of theme names</returns>
+        private static string[] LoadThemes()
+        {
+            FileStream _stream; // Create variable for the file stream
+
+            if (!File.Exists(_filePath)) // If the theme file doesn't exist
+            {
+                _stream = File.Create(_filePath); // Create the file
+                _stream.Close(); // Close the file
+                Load(); // Retry this function.
+            }
+
+            // If the file does exist
+            StreamReader _streamReader = new StreamReader(_filePath); // Open the text file.
+            string _fullText = _streamReader.ReadToEnd(); // Read the text file to the end.
+            _streamReader.Close(); // Close the text file.
+            return _fullText.Split("\n"); // Return the split text by the new line character
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Theme"/> from an ID
+        /// </summary>
+        /// <param name="id">The ID of the <see cref="Theme"/></param>
+        /// <returns></returns>
+        public static Theme GetTheme(int id)
+        {
+            return _themes[id];
+        }
+
+        /// <summary>
+        /// Adds a <see cref="Theme"/> to the list.
+        /// </summary>
+        /// <param name="_theme">Adds a <see cref="Theme"/> to the list</param>
+        public static void AddTheme(Theme _theme)
+        {
+            _themes.Add(_theme);
+            SaveThemes();
+        }
+    }
+
+    public class Theme 
+    {
+        private string _themeText = "";
+        public string ThemeName { get { return _themeText; } set { _themeText = value; } }
+
+        public Theme(string _themeName)
+        {
+            _themeText = _themeName;
+        }
+    }
+
+}
